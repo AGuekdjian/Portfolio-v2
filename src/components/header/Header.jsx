58c4babdf84449a63f8345109f1501831153b1db
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-01-14 14:19:05 -0300

    Terminando Home, Social, seccion de experiencia laboral agregado y correccion de estilos.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export const Header = () => {
-    const [activeNav, setActiveNav] = useState('#')
-    return (
-        <header className='block bg-slate-200/10 backdrop-blur-[1px] rounded-full py-4 px-3 w-fit fixed top-8 flex'>
-            <nav>
-                <ul className='flex'>
-                    <li className={`transition text-center mx-3 w-16 ${activeNav === "#" ? 'rounded-xl bg-cyan-500/30' : null}`} >
-                        <a href='#' onClick={() => setActiveNav('#')}>Inicio</a>
-                    </li>
-                    <li className={`transition text-center mx-3 w-24 ${activeNav === "#Proyectos" ? 'rounded-xl bg-cyan-500/30' : null}`}>
-                        <a href='#Proyectos' onClick={() => setActiveNav('#Proyectos')}>Proyectos</a>
-                    </li>
-                    <li className={`transition text-center mx-3 w-24 ${activeNav === "#Experiencia" ? 'rounded-xl bg-cyan-500/30' : null}`}>
-                        <a href='#Experiencia' onClick={() => setActiveNav('#Experiencia')}>Experience</a>
-                    </li>
-                    <li className={`transition text-center mx-3 w-20 ${activeNav === "#Contacto" ? 'rounded-xl bg-cyan-500/30' : null}`}>
-                        <a href='#Contacto' onClick={() => setActiveNav('#Contacto')}>Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <header className="block bg-slate-200/10 backdrop-blur-[1px] rounded-full py-4 px-3 w-fit fixed top-8 flex">
+      <nav>
+        <ul className="flex">
+          <li
+            className={`transition text-center mx-3 w-16 ${
+              activeNav === "#" ? "rounded-xl bg-cyan-700" : null
+            }`}
+          >
+            <a href="#" onClick={() => setActiveNav("#")}>
+              Inicio
+            </a>
+          </li>
+          <li
+            className={`transition text-center mx-3 w-24 ${
+              activeNav === "#Proyectos" ? "rounded-xl bg-cyan-700" : null
+            }`}
+          >
+            <a href="#Proyectos" onClick={() => setActiveNav("#Proyectos")}>
+              Proyectos
+            </a>
+          </li>
+          <li
+            className={`transition text-center mx-3 w-24 ${
+              activeNav === "#Experiencia" ? "rounded-xl bg-cyan-700" : null
+            }`}
+          >
+            <a href="#Experiencia" onClick={() => setActiveNav("#Experiencia")}>
+              Experience
+            </a>
+          </li>
+          <li
+            className={`transition text-center mx-3 w-20 ${
+              activeNav === "#Contacto" ? "rounded-xl bg-cyan-700" : null
+            }`}
+          >
+            <a href="#Contacto" onClick={() => setActiveNav("#Contacto")}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
--- src/components/social/Social.jsx
+++ src/components/social/Social.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
+import React from "react";
 
 export const Social = () => {
-    const info = [
-        {
-            name: 'Github',
-            url: '',
-            icon: 'fa-brands fa-github'
-        },
-        {
-            name: 'Linkedin',
-            url: '',
-            icon: 'fa-brands fa-linkedin'
-        },
-        {
-            name: 'Email',
-            url: 'mailto:anthonyguekdjian@gmail.com',
-            icon: "",
-        },
-        {
-            name: 'Whatsapp',
-            url: 'https://wa.me/+598098452454',
-            icon: ''
-        },
-        {
-            name: 'CV',
-            url: '',
-            icon: ''
-        },
-    ]
-    return (
-        <article>
-
-        </article>
-    )
-}
+  const info = [
+    {
+      name: "GitHub",
+      url: "https://github.com/AGuekdjian",
+      icon: "fa-brands fa-github",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/anthony-guekdjian/",
+      icon: "fa-brands fa-linkedin",
+    },
+    {
+      name: "anthonyguekdjian@gmail.com",
+      url: "mailto:anthonyguekdjian@gmail.com",
+      icon: "fa-regular fa-envelope",
+    },
+    {
+      name: "WhatsApp",
+      url: "https://wa.me/+598098452454",
+      icon: "fa-brands fa-whatsapp",
+    },
+    {
+      name: "Resume",
+      url: "",
+      icon: "fa-solid fa-briefcase",
+    },
+  ];
+  return (
+    <section className="mt-3 ml-1">
+      <nav className="flex">
+        {info.map((social, index) => {
+          return (
+            <a
+              key={index}
+              className="mx-1.5 rounded-full border border-white/10 flex justify-center items-center gap-x-3 px-4 bg-white/5 hover:scale-110 hover:bg-white/10 hover:cursor-pointer transition"
+              href={social.url}
+              target="_blank"
+            >
+              <i className={`${social.icon} text-2xl`}></i>
+              <span className="text-sm">{social.name}</span>
+            </a>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
